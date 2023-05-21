dc0c941ce60099fe6eb0f10c8c62191fd32960e7
Jean-Christophe Galvani <jchristophegalvani@gmail.com>
2023-05-21 08:19:24 -0400

    resize and center image

--- create_music_videos.py
+++ create_music_videos.py
@@ -1,12 +1,59 @@
-from moviepy.editor import AudioFileClip, ImageClip
 import os
-
-from lib.args import args
-from lib.write import create_dirs
-
 from tqdm import tqdm
+from lib.args import args
+from moviepy.editor import AudioFileClip, ImageClip, ColorClip, CompositeVideoClip
 
+height = 1080
+video_width = 1920
 
+def create_movie(path):
+    # Save music and image filenames    
+    audio_list = [os.path.join(path, file) for file in os.listdir(path) if file.split('.')[-1].lower() == 'mp3']
+    image_list = [os.path.join(path, file) for file in os.listdir(path) if file.split('.')[-1].lower() in ['png', 'jpg', 'jpeg']]
+    
+    if not audio_list or not image_list:
+        tqdm.write(f'missing image or music file to create a video in {path}')
+        return
+            
+    # Load the audio file
+    audio = AudioFileClip(audio_list[0])
+
+    # Load the image file
+    image = ImageClip(image_list[0])
+    original_width, original_height = image.size
+    new_width = int(original_width * height / original_height)
+    image = image.resize((new_width, height))
+
+    # Create a blank clip with the desired size
+    blank_clip = ColorClip((video_width, height), color=(0, 0, 0))
+
+    # Position the image in the center of the blank clip
+    x_pos = (video_width - image.w) // 2
+    y_pos = (height - image.h) // 2
+    image_clip = image.set_position((x_pos, y_pos))
+
+    # Create the video clip by combining the audio, image, and blank clips
+    video = CompositeVideoClip([blank_clip, image_clip.set_audio(audio)])
+    
+    # Set the duration of the video to match the audio duration
+    video = video.set_duration(audio.duration)
+    
+    # Write the video file
+    base = os.path.basename(image_list[0].split('.')[0])
+    dir = os.path.dirname(image_list[0].split('.')[0])
+    filename =os.path.join(dir, base)
+    video.write_videofile(f'{filename}.mp4', fps=1)
+    
+    
+if args.unique:
+        if not os.path.isdir(args.directories):
+            print(f'{args.directories} is not a directory')
+            exit()
+            
+        create_movie(args.directories)
+        exit()
+        
+        
 for directory in tqdm(os.listdir(args.directories), desc='directories', position=1):
     
     path = os.path.join(args.directories, directory)
@@ -14,26 +61,6 @@ for directory in tqdm(os.listdir(args.directories), desc='directories', position
         tqdm.write(f'{path} is not a directory')
         continue
     
-    # Save music and image filenames    
-    audio_list = [os.path.join(path, file) for file in os.listdir(path) if file.split('.')[-1] == 'mp3']
-    image_list = [os.path.join(path, file) for file in os.listdir(path) if file.split('.')[-1] in ['png', 'jpg', 'jpeg']]
+    create_movie(path)
     
-    if not audio_list or not image_list:
-        tqdm.write(f'missing image or music file to create a video in {path}')
-        continue
-            
-    # Load the audio file
-    audio = AudioFileClip(audio_list[0])
-
-    # Load the image file
-    image = ImageClip(image_list[0])
-
-    # Set the duration of the video to match the audio duration
-    duration = audio.duration
-
-    # Create the video clip with the image and audio
-    video = image.set_duration(duration).set_audio(audio)
-
-    # Write the video file
-    filename = image_list[0].split('.')[0]
-    video.write_videofile(f'{filename}.mp4', fps=1)
+    
