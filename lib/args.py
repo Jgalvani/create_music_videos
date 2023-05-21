21ced4f54de7ffe43fdd2bccaad35c4b026e49a9
Jean-Christophe Galvani <jchristophegalvani@gmail.com>
2023-05-21 08:25:44 -0400

    Update args.py

--- lib/args.py
+++ lib/args.py
@@ -1,6 +1,6 @@
 import argparse
 
 # Command line options
-parser = argparse.ArgumentParser(prog='change_tone', description='Create multiple tones for the same sound file')
+parser = argparse.ArgumentParser(prog='create_music_video', description='Create a video in a directory including a song file and an image file')
 parser.add_argument('-d', '--directories', type=str, help='sound file directory')
 args = parser.parse_args()
